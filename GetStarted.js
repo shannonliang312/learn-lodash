8cca753a0c9afc32c1842822e216b8e5c584c912
shannonliang <shannonliang312@gmail.com>
2017-02-11 21:58:29 +0800

    getting started:filter

---
+++ GetStarted.js
@@ -0,0 +1,9 @@
+var _ = require("lodash");
+
+var worker = function (arr) {
+  // do work; return stuff
+  return _.filter(arr, {active: true});
+};
+
+// export the worker function as a nodejs module
+module.exports = worker;
